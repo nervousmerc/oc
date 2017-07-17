258d497bd4e34f76df9ba57331ec66f38dd1f6cc
Nervous <nervousmerc@gmail.com>
2017-07-17 22:38:21 +0500

    add command sending to reactor.lua

--- reactor.lua
+++ reactor.lua
@@ -1,4 +1,5 @@
 local component = require("component")
+local shell = require("shell")
 local modem = component.modem
 
 local conf = {
@@ -10,7 +11,28 @@ local conf = {
 }
 
 for i = 1, 10 do
-  print("I will always make backups\n")
+  print("I will always make backups")
 end
+
+function sendCommand(command, address, port)
+  modem.broadcast(port, command)
+  print(("Sending %s to %s:%s"):format(command, address, port))
+end
+
+local args = shell.parse(...)
+
+if not args[1] then
+  print([[
+    Usage: reactor <COMMAND>
+      where COMMAND is one of "on", "off", "info", "status"]])
+  return
+end
+
+local address = conf.address or "any"
+local port = conf.port or 11
+local command = args[1]
+
+modem.open(port)
+sendCommand(command, address, port)
 
 

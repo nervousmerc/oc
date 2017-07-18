fcff1afda1e36c62107f6f3439396bf01c425724
Nervous <nervousmerc@gmail.com>
2017-07-18 21:16:07 +0500

    switch to buffered output

--- bin/monitor.lua
+++ bin/monitor.lua
@@ -1,16 +1,18 @@
 local component = require("component")
 local term = require("term")
 local os = require("os")
+local io = require("io")
 local energy = require("libenergy")
 local gpu = component.gpu
 
 gpu.setResolution(15, 5)
+io.stdout:setvbuf("full")
 
 while true do
   term.clear()
   for addr, name in pairs(energy.stores) do
-    print()
-    print(string.format(" %s %.0f", energy.getEnergy(addr, name)))
+    io.stdout:write((string.format("\n %s %.0f\n", energy.getEnergy(addr, name))))
   end
+  io.stdout:flush()
   os.sleep(2)
 end
